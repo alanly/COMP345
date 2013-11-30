c9d359a496b71b30ff30cba1358b1813628dc3b9
Alan Ly <me@alanly.ca>
2013-11-30 02:46:12 -0500

    Display player's current level.

--- DnDProject/GameEngineCharacterStats.cpp
+++ DnDProject/GameEngineCharacterStats.cpp
@@ -1,4 +1,6 @@
 #include "GameEngineCharacterStats.h"
+#include <iostream>
+#include <sstream>
 
 
 GameEngineCharacterStats::GameEngineCharacterStats(LoaderParameters* parameters, Character* character) : GameObject(parameters)
@@ -16,6 +18,26 @@ void GameEngineCharacterStats::handleMainEvents()
 }
 void GameEngineCharacterStats::draw()
 {
+	// Retrieve starting positions
+	int x = this->getParameters()->getXPos();
+	int y = this->getParameters()->getYPos();
+
+	// Draw Character Level
+	std::ostringstream convert;
+	convert << this->character->getLevel();
+	std::string levelMsg = "Current Level: " + convert.str();
+
+	GameObject* levelGameObj = new GameObject(new LoaderParameters(x,y,300,25,0,0,levelMsg));
+	SDL_Color color = {255,255,0};
+	TextureManager::getInstance()->loadFont(levelGameObj->getParameters()->getId(), Game::getInstance()->getRenderer(), levelGameObj->getParameters()->getId(), color);
+	levelGameObj->draw();
+	y += 25;
+
+
+	// Draw Character HP
+		// No consistent way to determine maximum HP.
+
+	// Draw Character Equipped Items
 	std::vector<Item*> characterEquipped = std::vector<Item*>();
 
 	characterEquipped.push_back(this->character->getHelmet());
@@ -27,9 +49,6 @@ void GameEngineCharacterStats::draw()
 	characterEquipped.push_back(this->character->getBoots());
 	characterEquipped.push_back(this->character->getBelt());
 
-	int x = this->getParameters()->getXPos();
-	int y = this->getParameters()->getYPos();
-
 	for (std::vector<Item*>::iterator cIt = characterEquipped.begin(); cIt != characterEquipped.end(); ++cIt) {
 		if (*cIt != nullptr) {
 			GameObject* itemGameObj = new GameObject(new LoaderParameters(x, y, 300, 25, 0, 0, (*cIt)->getName()));
@@ -40,7 +59,7 @@ void GameEngineCharacterStats::draw()
 		}
 	}
 
-	if (y == this->getParameters()->getYPos()) {
+	if (y == this->getParameters()->getYPos() + 25) {
 		GameObject* itemGameObj = new GameObject(new LoaderParameters(x, y, 300, 25, 0, 0, "No Items Equipped"));
 		SDL_Color color = {255, 255, 0};
 		TextureManager::getInstance()->loadFont(itemGameObj->getParameters()->getId(), Game::getInstance()->getRenderer(), itemGameObj->getParameters()->getId(), color);
