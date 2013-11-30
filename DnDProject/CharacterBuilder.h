98f2129911f064ef7a53c8c6c232ba19ebd671c9
Alan Ly <me@alanly.ca>
2013-11-30 09:33:55 -0500

    Initial Character with hit points properly.

--- DnDProject/CharacterBuilder.h
+++ DnDProject/CharacterBuilder.h
@@ -7,7 +7,11 @@ class CharacterBuilder
 	//Get the character
 	Character* getCharacter(){ return c; }
     //build a generic empty Pizza
-	void createNewCharacter(){ c = new Character("Player 1",1); }
+	void createNewCharacter() {
+		c = new Character("Player 1",1);
+		c->setCurrentHitPoints(100);
+		c->setMaxHitPoints(100);
+	}
     //Create the character
 	virtual void buildDefaultCharacteristics(string name, int level) = 0;
 	virtual void buildStats() = 0;
