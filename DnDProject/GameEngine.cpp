2ca7852b86a310cafcb6ee2a75c1ab639b948305
geoffrey-liu <geoffrey.p.liu@gmail.com>
2013-11-29 23:58:38 -0500

    Fighting and shit
    fighting and shit

--- DnDProject/GameEngine.cpp
+++ DnDProject/GameEngine.cpp
@@ -1,15 +1,30 @@
+#pragma once
 #include "GameEngine.h"
 #include "TextureManager.h"
 #include <iostream>
 #include "Game.h"
 #include "InputHandler.h"
+#include "Director.h"
+#include "TankCharacterBuilder.h"
+#include "AStar.h"
+using namespace Dice;
 
 GameEngine::GameEngine(LoaderParameters* parameters) : GameObject(parameters)
 {
 	gameUI = new GameObject(new LoaderParameters(0, 0, 960, 640,  0, 0, "gameUI"));
-	c = new Character("Player", 1);
-	map = new Map(7, 10, Position(1, 1), Position(5, 1));
-	mapView = new GameEngineMap(new LoaderParameters(16, 16, 0, 0, 0, 0, "gameEngineMap"), map, c);
+	Director* d = new Director();
+	TankCharacterBuilder* tank = new TankCharacterBuilder();
+	d->setCharacterBuilder(tank);
+	d->constructCharacter("RMT",1);
+	c = d->getCharacter();
+	ArmourItem* item = new ArmourItem("Armor","Desc");
+	c->addItemToInventory(item);
+	HelmetItem* hel = new HelmetItem("Helmet1","Desc");
+	c->addItemToInventory(hel);
+
+	map = new Map(20, 20, Position(1, 1), Position(5, 1));
+	mapView = new GameEngineMap(new LoaderParameters(16, 16, 0, 0, 0, 0, "gameEngineMap"), map, c, 1);
+	sideBar = new GameEngineSideBar(new LoaderParameters(720, 336, 0, 0, 0, 0, "gameEngineMap"),c);
 	currentView = GameEngineView::MAIN;
 
 	loadTextures();
@@ -21,7 +36,9 @@ GameEngine::~GameEngine()
 
 void GameEngine::drawMainView()
 {
+
 	gameUI->draw();
+	sideBar->draw();
 	mapView->draw();
 }
 
@@ -32,27 +49,73 @@ void GameEngine::handleMainEvents()
 	if (gameUI->isClicked())
 	{
 		Position click(mapView->getMouseClickedColumn(), mapView->getMouseClickedRow());
-		map->moveCharacter(click);
 		gameUI->resetClicked();
+
+		string s = pathFind(map->getMap(), map->getCharacterPosition().x, map->getCharacterPosition().y, click.x, click.y);
+
+		//cout<<s<<endl;
+
+		while (s.length() != 0)
+		{
+			cout << this->c->getCurrentHitPoints() << endl;
+			if (s.length() == 1)
+			{
+				vector<Monster> monsters = map->getMonsters();
+				for (int i = 0; i < monsters.size(); i++)
+				{
+					Monster monster = monsters[i];
+
+					if (click == monster.getPosition())
+					{
+						int initiative = Dice::roleTwentySideDice() + this->c->getAttackBonus();
+
+						if (initiative > monster.getArmorClass())
+						{
+							int damageDone = (this->c->getStrength()) + (this->c->getAttackBonus());
+							monster.setCurrentHitPoints((monster.getCurrentHitPoints()) - damageDone);
+						}
+
+						int damageTaken = (monster.getStrength()) + (monster.getAttackBonus());
+						this->c->setCurrentHitPoints((this->c->getCurrentHitPoints()) - damageTaken);
+						cout << "Character health: " << this->c->getCurrentHitPoints() << endl;
+						cout << "Monster health: " << monster.getCurrentHitPoints() << endl;
+					}
+				}
+				break;
+			}
+
+			SDL_RenderClear(Game::getInstance()->getRenderer());
+
+			map->moveCharacter((Direction)(s.front() - '0'));
+			gameUI->draw();
+			mapView->draw();
+
+			SDL_RenderPresent(Game::getInstance()->getRenderer());
+			SDL_Delay(100);
+			s = s.substr(1, s.length());
+		}
+		//map->moveCharacter(click);
 	}
 	string input = InputHandler::getInstance()->getInput();
 	if (input != "") {
 		char inputChar = input[0];
-
-		if (input == "W") {
-			//map->setCharacterPosition(Position(map->getCharacterPosition().x, (map->getCharacterPosition().y)-1));
-			map->moveCharacter(UP);
-		} else if (input == "A") {
-			//map->setCharacterPosition(Position((map->getCharacterPosition().x)-1, map->getCharacterPosition().y));
-			map->moveCharacter(LEFT);
-		} else if (input == "S") {
-			//map->setCharacterPosition(Position(map->getCharacterPosition().x, (map->getCharacterPosition().y)+1));
-			map->moveCharacter(DOWN);
-		} else if (input == "D") {
-			//map->setCharacterPosition(Position((map->getCharacterPosition().x)+1, map->getCharacterPosition().y));
-			map->moveCharacter(RIGHT);
+		//cout<< inputChar<<endl;
+		if (mapView->getMode() == 0) {
+			if (input == "W") {
+				//map->setCharacterPosition(Position(map->getCharacterPosition().x, (map->getCharacterPosition().y)-1));
+				map->moveCharacter(UP);
+			} else if (input == "A") {
+				//map->setCharacterPosition(Position((map->getCharacterPosition().x)-1, map->getCharacterPosition().y));
+				map->moveCharacter(LEFT);
+			} else if (input == "S") {
+				//map->setCharacterPosition(Position(map->getCharacterPosition().x, (map->getCharacterPosition().y)+1));
+				map->moveCharacter(DOWN);
+			} else if (input == "D") {
+				//map->setCharacterPosition(Position((map->getCharacterPosition().x)+1, map->getCharacterPosition().y));
+				map->moveCharacter(RIGHT);
+			}
+			//cout<<"["<<map->getCharacterPosition().x<<", "<<map->getCharacterPosition().y<<"]"<<endl;
 		}
-		//cout<<"["<<map->getCharacterPosition().x<<", "<<map->getCharacterPosition().y<<"]"<<endl;
 	}
 }
 
@@ -68,9 +131,19 @@ void GameEngine::draw() {
 		drawMainView();
 		break;
 	}
-	
 }
 
 void GameEngine::loadTextures() {
 	TextureManager::getInstance()->load("img/game/ui/game_ui.png", gameUI->getParameters()->getId(), Game::getInstance()->getRenderer());
+}
+
+void GameEngine::setCharacter(Character* character)
+{
+	sideBar->setCharacter(character);
+	mapView->setCharacter(character);
+	this->c = character;
+}
+void GameEngine::setMap(Map* map){
+	this->mapView->setMap(map);
+	this->map = map;
 }
